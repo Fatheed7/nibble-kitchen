cb3363fc038df37cf4c4f93a6312162d7af451e3
Fatheed7 <69724750+Fatheed7@users.noreply.github.com>
2022-06-20 19:09:18 +0100

    Imported fixtured, setup basic products and product detail pages

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Ingredients, Product, Sub_Categories 
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('sku', 'name', 'category', 'price', 'rating', 'image_url')
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name','name')
+
+class SubCategoriesAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name','name')
+
+class IngredientsAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name','name','is_allergen','has_sub_ingredient')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Sub_Categories, SubCategoriesAdmin)
+admin.site.register(Ingredients, IngredientsAdmin)
+admin.site.register(Product, ProductAdmin)
+
--- products/models.py
+++ products/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Sub_Categories(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Sub-Categories"
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Ingredients(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Ingredients"
+        
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    is_allergen = models.BooleanField(default=False, null=True, blank=True)
+    has_sub_ingredient = models.BooleanField(default=False, null=True, blank=True)
+    sub_ingredients = models.ManyToManyField('self', blank=True, null=True)
+    sub_category = models.ForeignKey(Sub_Categories, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    in_stock = models.BooleanField(default=False, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    ingredients = models.ForeignKey('Ingredients', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
