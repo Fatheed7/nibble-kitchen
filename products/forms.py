cafb55d024cf79d4cff7a01b655cd55da6d2d05a
Fatheed7 <69724750+Fatheed7@users.noreply.github.com>
2022-06-21 20:04:17 +0100

    Added functionality to add products to site

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Category
+from .models import Ingredients, Product, Category
 
 
 class ProductForm(forms.ModelForm):
@@ -12,7 +12,10 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+        ingredients = Ingredients.objects.all()
+        friendly_names_ingredients = [(i.id, i.get_friendly_name()) for i in ingredients]
 
         self.fields['category'].choices = friendly_names
+        self.fields['ingredients'].choices = friendly_names_ingredients
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
